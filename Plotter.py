7460dcb8814597272a6a926b842f17da6bd21f0b
Masai <mahapamasai@gmail.com>
2020-02-28 15:58:39 +0200

    end of week

---
+++ Plotter.py
@@ -0,0 +1,24 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+class Plotter:
+    def __init__(self, y, y_pred, residuals=None):
+        self.residuals= residuals
+        self.y= y
+        self.y_pred= y_pred
+    
+    def run_calculations(self):
+        resids= np.array(self.y)- np.array(self.y_pred)
+        return resids
+    
+    def plot(self):
+        if self.residuals is None:
+            self.residuals= self.run_calculations()
+        plt.hist(self.residuals)
+        plt.title('Residual distributions')
+        plt.xlabel('residuals')
+        plt.ylabel('count')
+
+
+
---
+++ ScatterPlotter.py
@@ -0,0 +1,16 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from Plotter import Plotter
+
+class ScatterPlotter(Plotter):
+    def __init__(self, y, y_pred):
+        super().__init__(y, y_pred)
+    def plot(self):
+        if self.residuals is None:
+            self.residuals= self.run_calculations()
+        plt.scatter(self.y_pred, self.residuals )
+        #plt.scatter(self.y, self.y_pred)
+        plt.title('Predictions vs actual values')
+        plt.ylabel('residuals')
+        plt.xlabel('predictions')
